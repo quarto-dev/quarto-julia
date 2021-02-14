0e9ab74d034955ec94c0b20b9486979b7f953b7d
JJ Allaire <jj@rstudio.com>
2021-02-14 14:38:51 -0500

    more args for render

--- src/Quarto.jl
+++ src/Quarto.jl
@@ -7,7 +7,17 @@ import JSON
 
 
 function render(input::AbstractString;
-                output_format::Union{Nothing,AbstractString} = nothing)
+                output_format::Union{Nothing,AbstractString} = nothing,
+                output_file::Union{Nothing,AbstractString} = nothing,
+                execute::Bool = true,
+                execute_dir::Union{Nothing,AbstractString} = nothing,
+                cache::Union{Nothing,Bool} = nothing,
+                cache_refresh::Bool = false,
+                kernel_keepalive::Union{Nothing,Int} = nothing,
+                kernel_restart::Bool = false,
+                debug::Bool = false,
+                quiet::Bool = false,
+                pandoc_args::Union{Nothing,Array{String}} = nothing)
 
   # quarto binary
   quarto_bin = Quarto.path()
@@ -23,6 +33,44 @@ function render(input::AbstractString;
   if !isnothing(output_format)
     push!(args, "--to")
     push!(args, output_format)
+  end
+  if !isnothing(output_file)
+    push!(args, "--output")
+    push!(args, output_file)
+  end
+  if execute 
+    push!(args, "--execute")
+  end
+  # TODO: execute params
+  if !isnothing(execute_dir)
+    push!(args, "--execute-dir")
+    push!(args, execute_dir)
+  end
+  if !isnothing(cache)
+    if cache
+      push!(args, "--cache")
+    else
+      push!(args, "--no-cache")
+    end
+  end
+  if cache_refresh
+    push!(args, "--cache-refresh")
+  end
+  if !isnothing(kernel_keepalive)
+    push!(args, "--kernel-keepalive")
+    push!(args, string(kernel_keepalive))
+  end
+  if kernel_restart
+    push!(args, "--kernel-restart")
+  end
+  if debug 
+    push!(args, "--debug")
+  end
+  if quiet 
+    push!(args, "--quiet")
+  end
+  if !isnothing(pandoc_args)
+    args = cat(args, pandoc_args, dims = 1)
   end
 
   # run render
